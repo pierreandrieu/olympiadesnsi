5a5bb72400fee936fff29543b0af9e891e9c0f56
andrieu <pierre.andrieu@lri.fr>
2023-12-27 16:14:30 +0100

    ajout des paramètres "static" pour le css

--- olympiadesnsi/settings.py
+++ olympiadesnsi/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = "olympiadesnsi.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        "DIRS": [os.path.join(BASE_DIR, 'olympiadesnsi/templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -143,7 +143,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
