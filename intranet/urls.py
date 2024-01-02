2515bc52f8cd779669d54b28da8b3d14e9013295
andrieu <pierre.andrieu@lri.fr>
2024-01-02 20:25:56 +0100

    ajouter une épreuve

--- intranet/urls.py
+++ intranet/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     # ... autres URLs ...
     path('participant/espace', views.espace_candidat, name='espace_candidat'),
-    path('organisateur/espace', views.espace_organisateur, name='espace_organisateur'),
-    path('organisateur/groupes', views.gestion_groupes, name='gestion_groupes'),
+    path('organisateur/espace', views.espace_organisateur, name='espace_organisateur'),    path('organisateur/groupes', views.gestion_groupes, name='gestion_groupes'),
     path('organisateur/epreuves', views.gestion_epreuves, name='gestion_epreuves'),
     path('organisateur/compte', views.gestion_compte, name='gestion_compte'),
     path('organisateur/creer-groupe', views.creer_groupe, name='creer_groupe'),
+    path('organisateur/creer-epreuve', views.creer_epreuve, name='creer_epreuve'),
 ]
--- intranet/views.py
+++ intranet/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group, User
 from django.http import HttpResponseForbidden
+from django.urls import reverse
+from django.contrib import messages
+from django.utils import timezone  # Pour la date actuelle
+from epreuve.models import GroupeCreePar, UserCreePar, Epreuve
+from .forms import EpreuveForm
 import random
 import string
 
@@ -10,10 +15,6 @@ import string
 def espace_candidat(request):
     # Ici, vous pouvez ajouter des logiques supplémentaires si nécessaire
     return render(request, 'intranet/espace_candidat.html')
-
-@login_required
-def espace_organisateur(request):
-    return render(request, 'intranet/espace_organisateur.html')
 
 
 @login_required
@@ -43,18 +44,35 @@ def creer_groupe(request):
         nom_groupe = request.POST.get('nom_groupe')
         nombre_utilisateurs = int(request.POST.get('nombre_utilisateurs'))
 
-        # Créer le groupe
-        groupe, existe_deja = Group.objects.get_or_create(name=nom_groupe)
+        # Vérifier si le groupe existe déjà
+        groupe, created = Group.objects.get_or_create(name=nom_groupe)
 
-        # ID du créateur du groupe (l'utilisateur actuellement connecté)
-        id_createur = request.user.id
+        if not created:
+            messages.error(request, 'Un groupe du même nom existe déjà.')
+            # Rediriger vers la même page avec un indicateur dans l'URL
+            return redirect(reverse('espace_organisateur') + '?openModal=true')
+
+        association = GroupeCreePar(
+            groupe=groupe,
+            createur=request.user,
+            date_creation=timezone.now()  # Date et l'heure actuelles
+        )
+        association.save()
 
         # Générer les utilisateurs
         for i in range(1, nombre_utilisateurs + 1):
-            username = f"part{id_createur}{groupe.id}{i}"
+            username = f"part{groupe.id:05d}_{i:03d}"
             password = generate_password()
-            user = User.objects.create_user(username=username, password=password)
-            groupe.user_set.add(user)
+            nouvel_utilisateur = User.objects.create_user(username=username, password=password)
+            groupe.user_set.add(nouvel_utilisateur)
+
+            # Créer une association UserCreePar
+            association_utilisateur = UserCreePar(
+                utilisateur=nouvel_utilisateur,  # Passer l'instance de User
+                createur=request.user,
+                date_creation=timezone.now()  # Date et l'heure actuelles
+            )
+            association_utilisateur.save()
 
         # Gérer la logique de téléchargement CSV ou stockage des mots de passe
 
@@ -62,11 +80,38 @@ def creer_groupe(request):
 
 
 @login_required
+def creer_epreuve(request):
+    if not request.user.groups.filter(name='Organisateur').exists():
+        return HttpResponseForbidden()
+    if request.method == 'POST':
+        form = EpreuveForm(request.POST)
+        if form.is_valid():
+            epreuve = form.save(commit=False)
+            epreuve.referent = request.user  # Définir l'utilisateur actuel comme référent
+
+            epreuve.save()
+            form.save_m2m()  # Important pour enregistrer les relations ManyToMany
+
+            messages.success(request, "L'épreuve a été créée avec succès.")
+            return redirect('espace_organisateur')
+        else:
+            messages.error(request, "Il y a eu une erreur dans la création de l'épreuve.")
+    else:
+        form = EpreuveForm()
+    return render(request, 'intranet/espace_organisateur.html', {'form': form})
+
+
+@login_required
 def espace_organisateur(request):
-    # Récupère les groupes associés à l'organisateur
-    groupes = Group.objects.filter(user=request.user)
-    return render(request, 'intranet/espace_organisateur.html', {'groupes': groupes})
+    # Récupère les groupes créés par l'utilisateur connecté
+    groupes_crees = GroupeCreePar.objects.filter(createur=request.user)
+    # Récupère les épreuves dont l'utilisateur est le référent
+    epreuves_crees = Epreuve.objects.filter(referent=request.user)
+    epreuve_form = EpreuveForm()
+    return render(request, 'intranet/espace_organisateur.html', {
+        'groupes_crees': groupes_crees, 'form': epreuve_form, 'epreuves_crees': epreuves_crees})
 
 
 def generate_password():
-    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(12))
+
