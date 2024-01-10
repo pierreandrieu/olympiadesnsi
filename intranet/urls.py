c3429b25a030664d9e91e7fed2e2f1bcdf3478b7
andrieu <pierre.andrieu@lri.fr>
2024-01-10 09:44:31 +0100

    ajout des liens pour téléchargement csv participants_id et mdp

--- intranet/urls.py
+++ intranet/urls.py
@@ -4,9 +4,10 @@ from . import views
 urlpatterns = [
     # ... autres URLs ...
     path('participant/espace', views.espace_candidat, name='espace_candidat'),
-    path('organisateur/espace', views.espace_organisateur, name='espace_organisateur'),    path('organisateur/groupes', views.gestion_groupes, name='gestion_groupes'),
-    path('organisateur/epreuves', views.gestion_epreuves, name='gestion_epreuves'),
+    path('organisateur/espace', views.espace_organisateur, name='espace_organisateur'),
     path('organisateur/compte', views.gestion_compte, name='gestion_compte'),
     path('organisateur/creer-groupe', views.creer_groupe, name='creer_groupe'),
     path('organisateur/creer-epreuve', views.creer_epreuve, name='creer_epreuve'),
+    path('organisateur/telecharger-csv', views.telecharger_csv, name='telecharger_csv'),
+    path('organisateur/afficher-telechargement', views.afficher_page_telechargement, name='afficher_page_telechargement'),
 ]
