67477e7f4904f55f6ddb3c3090698458ada6daa7
andrieu <pierre.andrieu@lri.fr>
2024-01-20 22:21:27 +0100

    urls pour gestion compte et mot de passe

--- intranet/urls.py
+++ intranet/urls.py
@@ -3,11 +3,14 @@ from . import views
 
 urlpatterns = [
     # ... autres URLs ...
-    path('participant/espace', views.espace_candidat, name='espace_candidat'),
+    path('participant/espace', views.espace_participant, name='espace_participant'),
     path('organisateur/espace', views.espace_organisateur, name='espace_organisateur'),
-    path('organisateur/compte', views.gestion_compte, name='gestion_compte'),
+    path('organisateur/compte', views.gestion_compte_organisateur, name='gestion_compte_organisateur'),
+    path('participant/compte', views.gestion_compte_participant, name='gestion_compte_participant'),
     path('organisateur/creer-groupe', views.creer_groupe, name='creer_groupe'),
     path('organisateur/creer-epreuve', views.creer_epreuve, name='creer_epreuve'),
     path('organisateur/telecharger-csv', views.telecharger_csv, name='telecharger_csv'),
     path('organisateur/afficher-telechargement', views.afficher_page_telechargement, name='afficher_page_telechargement'),
+    path('participant/compte/update', views.change_password_participant, name='change_password_participant'),
+    path('organisateur/compte/update', views.change_password_organisateur, name='change_password_organisateur'),
 ]
