e80fc8d809b67fde6e21d609bf3a93e48816e824
andrieu <pierre.andrieu@lri.fr>
2024-01-20 22:21:41 +0100

    chemins url plus pertinents

--- epreuve/urls.py
+++ epreuve/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     # ... autres URLs ...
-    path('organisateur/epreuve/<int:epreuve_id>/gerer', views.gerer_epreuve, name='gerer_epreuve'),
+    path('organisateur/gerer/<int:epreuve_id>/', views.gerer_epreuve, name='gerer_epreuve'),
     path('organisateur/inscrire-epreuves/<int:id_groupe>/', views.inscrire_epreuves, name='inscrire_epreuves'),
     path('organisateur/gerer-groupe/<int:id_groupe>/', views.gerer_groupe, name='gerer_groupe'),
-    path('epreuve/detail/<int:epreuve_id>/', views.detail_epreuve, name='detail_epreuve'),
-    path('epreuve/afficher/<int:epreuve_id>/', views.afficher_epreuve, name='afficher_epreuve'),
+    path('detail/<int:epreuve_id>/', views.detail_epreuve, name='detail_epreuve'),
+    path('afficher/<int:epreuve_id>/', views.afficher_epreuve, name='afficher_epreuve'),
     path('traiter_reponse_instance/', views.traiter_reponse_instance, name='traiter_reponse_instance'),
-    path('epreuve/traiter_reponse_code/<int:exercice_id>/', views.traiter_reponse_code, name='traiter_reponse_code'),
-    path('etat_exercices/<int:epreuve_id>/', views.etat_exercices, name='etat_exercices'),
+    path('traiter_reponse_code/<int:exercice_id>/', views.traiter_reponse_code, name='traiter_reponse_code'),
+    path('soumettre_reponse/', views.soumettre, name='soumettre'),
 ]
