4ea49961c2204f6b3e30e6456ad377f542c37e66
andrieu <pierre.andrieu@lri.fr>
2024-01-23 22:54:30 +0100

    ajout url pour visualiser ou supprimer une épreuve

--- epreuve/urls.py
+++ epreuve/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # ... autres URLs ...
     path('organisateur/gerer/<int:epreuve_id>/', views.gerer_epreuve, name='gerer_epreuve'),
     path('organisateur/inscrire-epreuves/<int:id_groupe>/', views.inscrire_epreuves, name='inscrire_epreuves'),
     path('organisateur/gerer-groupe/<int:id_groupe>/', views.gerer_groupe, name='gerer_groupe'),
@@ -11,7 +10,7 @@ urlpatterns = [
     path('soumettre_reponse/', views.soumettre, name='soumettre'),
     path('assigner-jeu-de-test/<int:exercice_id>/', views.assigner_jeu_de_test, name='assigner_jeu_de_test'),
     path('<int:epreuve_id>/ajouter_exercice/', views.ajouter_exercice, name='ajouter_exercice'),
-    path('<int:epreuve_id>/visualiser/', views.visualiser_epreuve, name='visualiser_epreuve'),
+    path('<int:epreuve_id>/visualiser/', views.visualiser_epreuve_organisateur, name='visualiser_epreuve'),
     path('<int:epreuve_id>/editer/', views.editer_epreuve, name='editer_epreuve'),
-    path('<int:epreuve_id>/supprimer/', views.supprimer_epreuve, name='supprimer_epreuve'),
+    path('<int:epreuve_id>/supprimer/', views.supprimer_epreuve, name="supprimer_epreuve"),
 ]
