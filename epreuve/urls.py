9797850c77bc622d152efe34957cc0630268f493
andrieu <pierre.andrieu@lri.fr>
2024-01-23 14:56:00 +0100

    nouvelles urls pour les épreuves

--- epreuve/urls.py
+++ epreuve/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('afficher/<int:epreuve_id>/', views.afficher_epreuve, name='afficher_epreuve'),
     path('soumettre_reponse/', views.soumettre, name='soumettre'),
     path('assigner-jeu-de-test/<int:exercice_id>/', views.assigner_jeu_de_test, name='assigner_jeu_de_test'),
+    path('<int:epreuve_id>/ajouter_exercice/', views.ajouter_exercice, name='ajouter_exercice'),
+    path('<int:epreuve_id>/visualiser/', views.visualiser_epreuve, name='visualiser_epreuve'),
+    path('<int:epreuve_id>/editer/', views.editer_epreuve, name='editer_epreuve'),
+    path('<int:epreuve_id>/supprimer/', views.supprimer_epreuve, name='supprimer_epreuve'),
 ]
