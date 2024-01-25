4164d2cc7e46d2477704bf84160a91eb872f8d99
andrieu <pierre.andrieu@lri.fr>
2024-01-25 10:45:13 +0100

    supprimer template inutile

--- epreuve/urls.py
+++ epreuve/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('organisateur/inscrire-epreuves/<int:id_groupe>/', views.inscrire_epreuves, name='inscrire_epreuves'),
-    path('organisateur/gerer-groupe/<int:id_groupe>/', views.gerer_groupe, name='gerer_groupe'),
     path('detail/<int:epreuve_id>/', views.detail_epreuve, name='detail_epreuve'),
     path('afficher/<int:epreuve_id>/', views.afficher_epreuve, name='afficher_epreuve'),
     path('soumettre_reponse/', views.soumettre, name='soumettre'),
-    path('assigner-jeu-de-test/<int:exercice_id>/', views.assigner_jeu_de_test, name='assigner_jeu_de_test'),
     path('<int:epreuve_id>/ajouter_exercice/', views.ajouter_exercice, name='ajouter_exercice'),
     path('<int:epreuve_id>/visualiser/', views.visualiser_epreuve_organisateur, name='visualiser_epreuve'),
     path('<int:epreuve_id>/editer/', views.editer_epreuve, name='editer_epreuve'),
@@ -15,5 +12,8 @@ urlpatterns = [
     path('<int:epreuve_id>/ajouter_orga/', views.ajouter_organisateur, name="ajouter_organisateur"),
     path('<int:epreuve_id>/inscrire_groupes_epreuve/', views.inscrire_groupes_epreuve, name="inscrire_groupe_epreuves"),
     path('exercice/editer/<int:id_exercice>/', views.editer_exercice, name='editer_exercice'),
-    path('exercice/supprimer/<int:id_exercice>/', views.supprimer_exercice, name='supprimer_exercice')
+    path('exercice/supprimer/<int:id_exercice>/', views.supprimer_exercice, name='supprimer_exercice'),
+    path('exercice/assigner-jeux-de-test/<int:id_exercice>/', views.assigner_jeux_de_test, name='assigner_jeux_de_test'),
+    path('exercice/redistribuer-jeux-de-test/<int:id_exercice>/', views.redistribuer_jeux_de_test,
+         name='redistribuer_jeux_de_test'),
 ]
