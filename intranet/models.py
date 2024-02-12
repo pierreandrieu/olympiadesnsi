7c600ebc766a12725e185e1e62d63a01bd4c335b
andrieu <pierre.andrieu@lri.fr>
2024-02-12 22:12:06 +0100

    déplacement du modèle GroupeCreePar

--- intranet/models.py
+++ intranet/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
-# Create your models here.
+
+class GroupeCreePar(models.Model):
+    groupe = models.ForeignKey(Group, related_name='associations_groupe_createur', on_delete=models.CASCADE)
+    createur = models.ForeignKey(User, related_name='groupes_crees', on_delete=models.CASCADE)
+    nombre_participants = models.IntegerField(default=0)
+    date_creation = models.DateField()
+
+    def __str__(self):
+        return f"{self.createur.username} -> {self.groupe.name}"
+
+    class Meta:
+        db_table = 'GroupeCreePar'
+        indexes = [
+            models.Index(fields=['createur']),
+        ]
+
+        unique_together = ['groupe', 'createur']
