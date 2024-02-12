bb1f14f11c0b3e4e766bde730713f37ec2807e3f
andrieu <pierre.andrieu@lri.fr>
2024-02-12 22:14:14 +0100

    mise à jour de la vue pour création d'épreuve, prise en compte de la possibilité d'inscriptions externes

--- intranet/views.py
+++ intranet/views.py
@@ -6,14 +6,16 @@ from django.urls import reverse
 from django.shortcuts import render
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
-from epreuve.models import User, GroupeCreePar, Epreuve, GroupeParticipeAEpreuve, UserEpreuve, MembreComite, Exercice
-from django.http import HttpResponse
-from django.db.models import Q, Count, Prefetch
-from intranet.tasks import save_users_task  # La tâche Celery pour sauvegarder les utilisateurs
-from epreuve.forms import EpreuveForm
 from django.utils.crypto import get_random_string
 from django.utils import timezone
 from django.contrib import messages
+from django.http import HttpResponse
+from django.db.models import Q, Count, Prefetch
+from epreuve.models import User, Epreuve, UserEpreuve, MembreComite, Exercice
+from inscription.models import GroupeParticipeAEpreuve, Inscription_domaine
+from intranet.models import GroupeCreePar
+from intranet.tasks import save_users_task
+from epreuve.forms import EpreuveForm
 
 
 @login_required
@@ -99,13 +101,13 @@ def creer_groupe(request):
         # nombre_groupes = GroupeCreePar.objects.filter(createur_id=request.user.id).count()
         user_data = ["Username,Password"]
         users_info = []
-        for i in range(1, nombre_utilisateurs+1):
+        for i in range(1, nombre_utilisateurs + 1):
             username = get_unique_username(id_createur, i)
             password = get_random_string(length=12,
                                          allowed_chars='abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789')
             users_info.append((username, password))
             user_data.append(f"{username},{password}")
-    # Supposons que l'association GroupeCreePar doit être créée une seule fois par groupe
+        # Supposons que l'association GroupeCreePar doit être créée une seule fois par groupe
 
         # Appeler la tâche Celery pour sauvegarder les utilisateurs
         save_users_task.delay(nom_groupe, users_info, request.user.id)
@@ -129,13 +131,13 @@ def supprimer_groupe(request, groupe_id):
     if request.method == "POST":
         # Récupérer tous les utilisateurs du groupe
         users_du_groupe = groupe.user_set.all()
-        print("users = ", users_du_groupe)
         try:
 
             for user in users_du_groupe:
-            # Vérifier si l'utilisateur appartient à d'autres groupes.
-            # Par defaut, dans groupe "Participant" et dans le groupe nommé par l'organisateur
-                if user.groups.count() == 2 and user.groups.filter(name="Participant").exists():                # Si l'utilisateur n'appartient à aucun autre groupe, le supprimer
+                # Vérifier si l'utilisateur appartient à d'autres groupes.
+                # Par defaut, dans groupe "Participant" et dans le groupe nommé par l'organisateur
+                if user.groups.count() == 2 and user.groups.filter(name="Participant").exists():
+                    # Si l'utilisateur n'appartient à aucun autre groupe, le supprimer
                     user.delete()
 
             groupe.delete()
@@ -143,6 +145,7 @@ def supprimer_groupe(request, groupe_id):
         except:
             messages.error(request, "Une erreur s'est produite pendant la suppression.")
         return redirect('espace_organisateur')
+
 
 def telecharger_csv(request):
     if not request.user.groups.filter(name='Organisateur').exists():
@@ -159,7 +162,6 @@ def telecharger_csv(request):
     return response
 
 
-# Dans views.py
 def afficher_page_telechargement(request):
     if not request.user.groups.filter(name='Organisateur').exists():
         return HttpResponseForbidden()
@@ -174,20 +176,29 @@ def creer_epreuve(request):
         form = EpreuveForm(request.POST)
         if form.is_valid():
             epreuve = form.save(commit=False)
-            epreuve.referent = request.user  # Définir l'utilisateur actuel comme référent
+            # Utilisateur actuel est référent
+            epreuve.referent = request.user
             epreuve.save()
             form.save_m2m()  # Important pour enregistrer les relations ManyToMany
 
             # Ajouter le référent au comité d'organisation
             MembreComite.objects.create(epreuve=epreuve, membre=request.user)
 
+            # Traitement des domaines autorisés
+            domaines_str = request.POST.get('domaines_autorises', '')
+            domaines_list = [d.strip() for d in domaines_str.split('\n') if d.strip().startswith('@')]
+            Inscription_domaine.objects.filter(epreuve=epreuve).delete()
+            for domaine in domaines_list:
+                Inscription_domaine.objects.create(epreuve=epreuve, domaine=domaine)
+
             messages.success(request, "L'épreuve a été créée avec succès.")
             return redirect('espace_organisateur')
         else:
             messages.error(request, "Il y a eu une erreur dans la création de l'épreuve.")
+            return redirect('espace_organisateur')
     else:
         form = EpreuveForm()
-    return render(request, 'intranet/espace_organisateur.html', {'form': form})
+    return render(request, 'intranet/creer_epreuve.html', {'form': form})
 
 
 @login_required
@@ -232,67 +243,10 @@ def espace_organisateur(request):
     })
 
 
-""""
-@login_required
-def espace_organisateur(request):
-    if not request.user.groups.filter(name='Organisateur').exists():
-        return HttpResponseForbidden()
-
-    user = request.user
-
-    # Récupère les groupes créés par l'utilisateur avec le nombre de membres pour chaque groupe
-    groupes_crees = GroupeCreePar.objects.filter(createur=user) \
-        .annotate(nombre_membres=Count('groupe__user'))
-
-    # Récupère les épreuves dont l'utilisateur est le référent ou membre du comité
-    epreuves_organisees = Epreuve.objects.filter(
-        Q(referent=user) | Q(comite=user)
-    ).distinct()
-
-    # Préparation des données pour les épreuves et les exercices
-    epreuves_info = []
-    for epreuve in epreuves_organisees:
-        exercices_info = []
-        for exercice in epreuve.exercice_set.all():
-            # Vérifier si l'exercice utilise des jeux de test
-            if exercice.avec_jeu_de_test:
-                # Calculer le nombre de participants sans jeu de test
-                nb_participants_sans_jeu = UserExercice.objects.filter(
-                    exercice=exercice,
-                    jeu_de_test__isnull=True
-                ).count()
-                # Calculer le nombre de jeux de tests
-                nb_jeux_tests = JeuDeTest.objects.filter(exercice=exercice).count()
-            else:
-                # -1 pour indiquer que les jeux de test ne sont pas applicables
-                nb_participants_sans_jeu = -1
-                nb_jeux_tests = -1
-
-            # Récupérer le nom d'utilisateur de l'auteur de l'exercice
-            nom_auteur = exercice.auteur.username if exercice.auteur else "Inconnu"
-
-            exercices_info.append((exercice, nb_participants_sans_jeu, nb_jeux_tests, nom_auteur))
-
-        # Calculer le nombre total de participants uniques dans les groupes associés à chaque épreuve
-        groupes_participants = epreuve.groupes_participants.all()
-        participants_uniques = User.objects.filter(groups__in=groupes_participants).distinct().count()
-
-        epreuves_info.append((epreuve, exercices_info, participants_uniques))
-
-    epreuve_form = EpreuveForm()
-
-    return render(request, 'intranet/espace_organisateur.html', {
-        'groupes_crees': groupes_crees,
-        'epreuves_info': epreuves_info,
-        'form': epreuve_form
-    })
-"""
-
-
 def get_unique_username(id_user: int, num: int):
     partie_alea = get_random_string(length=3, allowed_chars='abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ')
     faux_id: str = str(2 * id_user + 100)
-    return f"{partie_alea}{faux_id[:len(faux_id)//2]}_{num:03d}{faux_id[len(faux_id)//2:]}"
+    return f"{partie_alea}{faux_id[:len(faux_id) // 2]}_{num:03d}{faux_id[len(faux_id) // 2:]}"
 
 
 @login_required
