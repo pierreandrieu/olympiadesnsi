eb112e76df1ca6890d91b3d611f1d8b2b26a3923
andrieu <pierre.andrieu@lri.fr>
2024-02-12 22:21:44 +0100

    mise à jour de la création de groupes de participants : prise en compte de la suppression d'une table non utilisée

--- intranet/tasks.py
+++ intranet/tasks.py
@@ -1,6 +1,6 @@
 from celery import shared_task
 from django.contrib.auth.models import User, Group
-from epreuve.models import UserCreePar, GroupeCreePar
+from intranet.models import GroupeCreePar
 from django.utils import timezone
 from django.db import transaction
 import logging
@@ -26,14 +26,9 @@ def save_users_task(nom_groupe, users_info, user_id):
                 User.objects.bulk_create(users)
                 groupe.user_set.add(*users)
                 groupe_participants.user_set.add(*users)
-                user_creepar_associations = [UserCreePar(utilisateur=user,
-                                                         createur=createur,
-                                                         date_creation=timezone.now()) for
-                                             user in users]
-                UserCreePar.objects.bulk_create(user_creepar_associations)
+
             except Exception as e:
-                # Loguez l'erreur ici
-                print(f"Erreur lors de la création des utilisateurs : {e}")
+                logger.error(f"Erreur lors de la création des utilisateurs : {e}")
 
             if not GroupeCreePar.objects.filter(groupe=groupe).exists():
                 GroupeCreePar.objects.create(groupe=groupe,
