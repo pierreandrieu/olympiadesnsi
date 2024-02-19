0fb5666e289550b4886ff5e2c019ea4cf99bdc13
andrieu <pierre.andrieu@lri.fr>
2024-02-19 15:32:00 +0100

    utilisation de constantes plutôt que de valeurs en dur

---
+++ olympiadesnsi/constants.py
@@ -0,0 +1,15 @@
+# Authentification et Sécurité
+TOKEN_LENGTH = 50
+NB_TENTATIVES_CONNEXIONS_AVANT_CAPTCHA = 3
+MAX_USERNAME_TAILLE = 100
+HEURES_VALIDITE_TOKEN = 1
+TAILLE_MDP = 12
+
+# Paramètres des Groupes
+MAX_USERS_PAR_GROUPE = 999
+
+# Paramètres des Exercices
+DEFAULT_MAX_SOUMISSIONS_PAR_EXERCICE = 50
+
+# Paramètres des Epreuves
+MAX_TAILLE_NOM = 100
