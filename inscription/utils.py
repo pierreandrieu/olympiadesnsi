85c3e694edb240ce633f208a7897337a9140d6e4
andrieu <pierre.andrieu@lri.fr>
2024-03-19 03:20:39 +0100

    fonction de creation de participants n'utilise plus celery

--- inscription/utils.py
+++ inscription/utils.py
@@ -1,7 +1,9 @@
 import logging
+from datetime import datetime
 from typing import List, Iterable, Optional
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from django.core.mail import EmailMessage
 from django.db import transaction
 from django.db.models import QuerySet
 from django.utils.crypto import get_random_string
@@ -10,6 +12,8 @@ from epreuve.models import Exercice, Epreuve, UserEpreuve, UserExercice
 from epreuve.utils import assigner_participants_jeux_de_test
 
 from inscription.models import GroupeParticipeAEpreuve, GroupeParticipant, InscriptionExterne, InscripteurExterne
+from intranet.models import ParticipantEstDansGroupe
+from olympiadesnsi import settings
 from olympiadesnsi.constants import TOKEN_LENGTH
 
 
@@ -116,3 +120,92 @@ def calculer_nombre_inscrits(epreuve: Epreuve, inscripteur_externe: InscripteurE
         total_participants += groupe.get_nombre_participants()
 
     return total_participants
+
+
+def save_users(groupe_id: int, usernames: List[str],
+               inscription_externe_id: Optional[int] = None) -> dict:
+    """
+    Crée et enregistre des utilisateurs en masse (sans mot de passe) associe à un groupe de participants
+    et les inscrit à une épreuve si spécifié.
+
+    Args:
+        groupe_id (int): L'id du groupe de participants.
+        usernames (List[str]): Liste des noms d'utilisateurs à ajouter.
+        inscription_externe_id (Optional[int]): L'id de l'inscription externe si les participants sont créés par
+        un inscripteur externe.
+
+    Returns:
+        dict: Un dictionnaire indiquant le statut de l'opération.
+    """
+    try:
+        groupe: GroupeParticipant = GroupeParticipant.objects.get(id=groupe_id)
+    except GroupeParticipant.DoesNotExist:
+        return {'status': 'error', 'message': f"Groupe {groupe_id} introuvable"}
+    try:
+        # Début d'une transaction pour garantir l'intégrité des données
+        with transaction.atomic():
+            # Récupération du groupe de participants par son identifiant
+            groupe.statut = "CREATION"
+
+            groupe.save()
+
+            # Création des objets User sans les sauvegarder immédiatement dans la base de données
+            users: List[User] = [User(username=username) for username in usernames]
+
+            # Enregistrement en masse des objets User créés
+            User.objects.bulk_create(users)
+
+            # Mise à jour des instances User avec leurs ID après la création
+            users: QuerySet[User] = User.objects.filter(username__in=[user.username for user in users])
+
+            # Association des utilisateurs au groupe "Participant" standard
+            groupe_participant: Group = Group.objects.get(name="Participant")
+            UserGroupRelations = User.groups.through
+            user_group_relations = [UserGroupRelations(user_id=user.id, group_id=groupe_participant.id) for user in
+                                    users]
+            UserGroupRelations.objects.bulk_create(user_group_relations)
+
+            # Association des utilisateurs au groupe de participants personnalisé
+            membres: List[ParticipantEstDansGroupe] = [ParticipantEstDansGroupe(utilisateur=user, groupe=groupe)
+                                                       for user in users]
+            ParticipantEstDansGroupe.objects.bulk_create(membres)
+
+            email: Optional[str] = None
+            epreuve_nom: Optional[str] = None
+            # Inscription du groupe à l'épreuve si un identifiant d'épreuve est fourni
+            if inscription_externe_id:
+                inscription_externe: InscriptionExterne = InscriptionExterne.objects.get(id=inscription_externe_id)
+                # Logique pour inscrire le groupe à l'épreuve et aux exercices associés
+                inscrire_groupe_a_epreuve(groupe=groupe, epreuve=inscription_externe.epreuve, participants=users)
+                email = inscription_externe.inscripteur.email
+                epreuve_nom = inscription_externe.epreuve.nom
+                mail = EmailMessage(
+                    subject=f"Inscription à {inscription_externe.epreuve.nom}",
+                    body=f"Veuillez trouver ci-joint les identifiants "
+                         f"des {len(usernames)} équipes. "
+                         f"Les mots de passe devront être définis à la première connexion.\n"
+                         f"En cas de perte de mot de passe par une équipe, vous pourrez utiliser l'onglet de "
+                         f"récupération de compte sur la page principale des olympiades.",
+                    from_email=settings.EMAIL_HOST_USER,
+                    to=[email],
+                )
+
+                # Ajout de la pièce jointe
+                mail.attach(
+                    filename=f"identifiants_participants_{epreuve_nom}_{datetime.now().strftime('%d-%m_%H-%M')}",
+                    content="Nom d'utilisateur\n" +
+                            "\n".join([f"{user}" for user in usernames]),
+                    mimetype='text/csv')
+
+                # Envoi de l'email
+                mail.send()
+
+            groupe.statut = 'VALIDE'
+            groupe.save()
+            return {'status': 'success', 'email': email, 'epreuve_nom': epreuve_nom, 'usernames': usernames}
+
+    except Exception as e:
+        # En cas d'erreur, mise à jour du statut du groupe et enregistrement de l'erreur
+        groupe.statut = 'ECHEC'
+        groupe.save()
+        return {'status': 'error', 'message': str(e)}
