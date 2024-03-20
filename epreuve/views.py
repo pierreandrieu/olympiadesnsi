c68230cc822c5d5a1e6ebdd65fa79c74fb5e3eea
andrieu <pierre.andrieu@lri.fr>
2024-03-20 02:04:13 +0100

    suppression import désuet

--- epreuve/views.py
+++ epreuve/views.py
@@ -2,7 +2,6 @@ import logging
 from random import choice
 
 from django.contrib.auth.models import User
-from django.core.serializers import serialize
 from django.db import transaction
 from django.db.models import QuerySet
 from django.shortcuts import get_object_or_404
