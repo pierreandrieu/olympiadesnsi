78abcebd8bbd1ddccc36688b28bc0bb0aa4a9bd8
andrieu <pierre.andrieu@lri.fr>
2024-03-20 09:01:08 +0100

    ne modifie pas le mot de passe si l'utilisateur existe deja et ignore simplement l'ajout

--- olympiadesnsi/management/commands/ajouteorganisateurs.py
+++ olympiadesnsi/management/commands/ajouteorganisateurs.py
@@ -27,12 +27,12 @@ class Command(BaseCommand):
                     username = cols[0]
                     password = cols[1].strip()
                     user, created = User.objects.get_or_create(username=username)
-                    user.set_password(password)
-                    user.save()
                     if created:
+                        user.set_password(password)
+                        user.save()
                         groupe_organisateurs.user_set.add(user)
                         self.stdout.write(self.style.SUCCESS(f'Organisateur {username} créé avec succès'))
                     else:
-                        self.stdout.write(self.style.SUCCESS(f"Mot de passe de l'organisateur {username} modifié avec succès"))
+                        self.stdout.write(self.style.SUCCESS(f"organisateur {username} existe déjà"))
 
 
