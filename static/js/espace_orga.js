093a3f23901711234b2fd77664b6f57c4e5d15c7
andrieu <pierre.andrieu@lri.fr>
2024-04-27 11:32:40 +0200

    datatable en français

--- static/js/espace_orga.js
+++ static/js/espace_orga.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
-    $('#tableGroupes').DataTable();
+    $('#tableGroupes').DataTable({
+        "language": {
+            "lengthMenu": "Afficher _MENU_ éléments par page",
+            "zeroRecords": "Aucun élément trouvé",
+            "info": "Affichage de la page _PAGE_ sur _PAGES_",
+            "infoEmpty": "Aucun enregistrement disponible",
+            "infoFiltered": "(filtré de _MAX_ enregistrements au total)",
+            "search": "Recherche:",
+            "paginate": {
+                "first": "Premier",
+                "last": "Dernier",
+                "next": "Suivant",
+                "previous": "Précédent"
+            },
+            "aria": {
+                "sortAscending": ": activer pour trier la colonne par ordre croissant",
+                "sortDescending": ": activer pour trier la colonne par ordre décroissant"
+            }
+        }
+    });
 });
