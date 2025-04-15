8dbbfa79bffc048f9b5a23eb497b454fdab9293b
andrieu <pierre.andrieu@lri.fr>
2025-04-15 15:49:46 +0200

    ajout related_name avec Exercice dans modèle JeuDeTest

--- epreuve/models/jeudetest.py
+++ epreuve/models/jeudetest.py
@@ -1,20 +1,17 @@
-from typing import TYPE_CHECKING
-
 from django.db import models
-
-if TYPE_CHECKING:
-    from epreuve.models.exercice import Exercice
 
 
 class JeuDeTest(models.Model):
-    exercice = models.ForeignKey('Exercice', on_delete=models.CASCADE)
+    exercice = models.ForeignKey(
+        'Exercice',
+        on_delete=models.CASCADE,
+        related_name='jeux_de_test'
+    )
     instance = models.TextField(null=False)
     reponse = models.TextField(null=False)
 
     class Meta:
         db_table = 'JeuDeTest'
-
         indexes = [
             models.Index(fields=['exercice'])
         ]
-
