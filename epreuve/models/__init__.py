4554ed291b2d7b4ef7f185813426ebedd9461515
andrieu <pierre.andrieu@lri.fr>
2025-04-15 09:42:36 +0200

    fichiers init

---
+++ epreuve/models/__init__.py
@@ -0,0 +1,15 @@
+from .epreuve import Epreuve
+from .exercice import Exercice
+from .membrecomite import MembreComite
+from .userepreuve import UserEpreuve
+from .userexercice import UserExercice
+from .jeudetest import JeuDeTest
+
+__all__ = [
+    "Epreuve",
+    "Exercice",
+    "MembreComite",
+    "UserEpreuve",
+    "UserExercice",
+    "JeuDeTest",
+]
